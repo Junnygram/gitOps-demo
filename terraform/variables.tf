aaa605174779560f5c5f67ab4d944928db5fbd98
Junnygram <you@example.com>
2025-04-20 13:05:48 +0100

    ft

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "name" {
+  default = "gitOpsdemo-eks"
+}
+
+variable "ecr_repo" {
+  default = "gitops-webappp"
+}
+
+variable "k8s_version" {
+  default = "1.31"
+}
+
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+variable "private_subnet_cidr_blocks" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+variable "public_subnet_cidr_blocks" {
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "tags" {
+  default = {
+    App = "eks-cluster"
+  }
+}
+
+
+#
